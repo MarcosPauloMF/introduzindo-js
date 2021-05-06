da8439a4e0ee3748a74710378da71fe6cc6aa0e7
Marcos <marcos.mpmf43@gmail.com>
2021-05-06 15:34:18 -0300

    Fazendo IMC em alguns campos funcionar

--- js/principal.js
+++ js/principal.js
@@ -1,37 +1,45 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = "nutrição legal"
 
-var paciente = document.querySelector("#primeiro-paciente")
+var paciente = document.querySelectorAll(".paciente")
+console.log(paciente)
 
-var tdPeso = paciente.querySelector(".info-peso")
-var peso = tdPeso.textContent
+for(var i = 0; i < paciente.length; i++){
+    
+    var paciente = paciente[1]
 
-var tdAltura = paciente.querySelector(".info-altura")
-var altura = tdAltura.textContent
+    var tdPeso = paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent
+    
+    var tdAltura = paciente.querySelector(".info-altura")
+    var altura = tdAltura.textContent
+    
+    var tdImc = paciente.querySelector(".info-imc")
+    
+    
+    var pesoEhValido = true
+    var alturaEhValida = true
+    
+    if (peso <= 0 || peso >= 1000){
+        console.log("peso invalido")
+        pesoEhValido = false
+        tdImc.textContent = "peso inválido"
+    }
+    
+    
+    if (altura <= 0 || altura >= 4){
+        console.log("altura invalido")
+        alturaEhValida = false
+        tdImc.textContent = "Altura invalida"
+    }
+    
+    
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura)
+        tdImc.textContent = imc.toFixed(2)
+    }
 
-var tdImc = paciente.querySelector(".info-imc")
-
-
-var pesoEhValido = true
-var alturaEhValida = true
-
-if (peso <= 0 || peso >= 1000){
-    console.log("peso invalido")
-    pesoEhValido = false
-    tdImc.textContent = "peso inválido"
 }
 
-
-if (altura <= 0 || peso >= 4){
-    console.log("altura invalido")
-    alturaEhValida = false
-    tdImc.textContent = "Altura invalida"
-}
-
-
-if (alturaEhValida && pesoEhValido)
-var imc = peso / (altura * altura)
-
-tdImc.textContent = imc
 
 

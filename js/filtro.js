cad8e30f98cfdf725fda16513786ede5eb372ec0
Marcos <marcos.mpmf43@gmail.com>
2021-06-01 11:06:55 -0300

    Adicionando lógica no filtro e retornando valores

---
+++ js/filtro.js
@@ -0,0 +1,44 @@
+var campoFiltro = document.querySelector("#filtrar-tabela")
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente")
+
+    if (this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            var tdNome = paciente.querySelector(".info-nome")   //buscando nome de paciente dentro de td em html
+            var nome = tdNome.textContent
+            if( nome != this.value){
+                paciente.classList.add("invisivel")
+            }else{
+                paciente.classList.remove("invisivel")
+            }
+        }
+   } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+})
+
+/* var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+
+    var pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i < pacientes.length; i++) {
+        var paciente = pacientes[i];
+        var tdNome = paciente.querySelector(".info-nome");
+        var nome = tdNome.textContent;
+        //nesse if eu já resolvo os problemas.
+        if (nome.toLowerCase() != this.value.toLowerCase() && this.value.length > 0) {
+            paciente.classList.add('invisivel');
+        } else {
+            paciente.classList.remove('invisivel');
+        }
+    }
+}); */
